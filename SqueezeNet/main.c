3a8bd4c236b76f71bd167563898aef12b8dce12a
Thomas Hou <hytcqq@gmail.com>
2019-08-29 15:09:51 +0800

    Initial Commit

---
+++ SqueezeNet/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SqueezeNet
+//
+//  Created by Thomas Hou on 2019/8/29.
+//  Copyright © 2019 Thomas Hou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
