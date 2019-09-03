48447b52f52fcc885c09b09dd9d402e75e863495
Yt Hou <hytcqq@gmail.com>
2019-09-03 22:33:56 +0800

    make layer out width & height right

--- SqueezeNet/main.c
+++ SqueezeNet/main.c
@@ -43,6 +43,8 @@ typedef struct layer{
     uint16_t w;
     uint16_t h;
     /* filters / out channel */
+    uint16_t wo;
+    uint16_t ho;
     uint16_t co;
     /* convolution / pooling kernel */
     uint8_t k;
@@ -78,19 +80,19 @@ typedef struct network {
     // weight + [layer: in(image in 1st layer / last layer's out) + out(next layer's in)]
     // weight lenght: [cout * cin * kernel^2]
     // bias lenght: [cout]
-    float data[1024*1024*40]; // in DRAM
+    float data[1024*1024*20]; // in DRAM
     float *dw;
 }network;
 
 void print_layer(layer *l) {
-    printf("%5s, %3d, %3d, %4d, %4d, %3d, %3d, %d, %2d, %d, %6d, %7d, %7d, %7d\n", l->name,
-           l->w, l->h, l->c, l->co, l->w/l->s, l->h/l->s, l->k, l->s, l->pad,
-           l->weight, l->din, l->dout, l->co * (l->w/l->s) * (l->h/l->s));
+    printf("%5s, %3d, %3d, %4d, %4d, %3d, %3d, %2d, %2d, %d, %6d, %7d, %7d, %7d\n", l->name,
+           l->w, l->h, l->c, l->co, l->wo, l->ho, l->k, l->s, l->pad,
+           l->weight, l->din, l->dout, l->co * (l->wo) * (l->ho));
 }
 
 void print_wetwork(network* net) {
     int i = 0;
-    printf(" i,  type, win, hin,  cin, cout,wout,hout, k,  s, p, weight, data in, dataout, data len\n");
+    printf(" i,  type, win, hin,  cin, cout,wout,hout,  k,  s, p, weight, data in, dataout, data len\n");
     for (i=0; i<net->llen; i++) {
         printf("%2d: ", i);
         print_layer(net->layers + i);
@@ -111,10 +113,12 @@ layer* build_layer(network* net, uint32_t type, uint16_t co, uint8_t k, uint8_t 
     else {
         l->in = l-in;
         l->c = l->in->co;
-        l->w = l->in->w / l->in->s;
-        l->h = l->in->h / l->in->s;
+        l->w = l->in->wo;
+        l->h = l->in->ho;
         l->din = l->in->dout;
     }
+    l->ho = (l->h + 2 * pad - k) / s + 1;
+    l->wo = (l->w + 2 * pad - k) / s + 1;
     l->co = co;
     l->k = k;
     l->s = s;
@@ -146,8 +150,7 @@ layer* build_layer(network* net, uint32_t type, uint16_t co, uint8_t k, uint8_t 
 // image pixel channle, todo PAD
 float input(layer *l, float *d, uint16_t w, uint16_t h, uint8_t c){
     // ignore pad
-//    w -= l->k/2;
-//    h -= l->k/2;
+
     if(w > l->w)    w = l->w - 1;
     else if(w < 0)  w = 0;
     
@@ -166,8 +169,8 @@ void conv_layer(network* net, layer *l) {
     float *wei =(float*)(net->dw);
     float *bias=(float*)(net->dw+l->weight);
     for (co=0; co<l->co; co++) {
-        for (h=0; h<l->h/l->s; h++) {
-            for (w=0; w<l->w/l->s; w++) { // stride
+        for (h=0; h<l->ho; h++) {
+            for (w=0; w<l->wo; w++) { // stride
                 float val = bias[co]; // add bias
                 for (c=0; c<l->c; c++) {
                     // convolution with kernel
@@ -175,7 +178,7 @@ void conv_layer(network* net, layer *l) {
                         for (kw=0; kw<l->k; kw++) {
                             float weight = wei[co*l->c*tk + c*tk + l->k*kh + kw];
                             float p = input(l, din, l->s*w+kw, l->s*h+kh, c);
-//                            printf("%f, %f\n", p, weight);
+                           // printf("%f, %f\n", p, weight);
                             val +=  p * weight;
                         }
                     }
@@ -186,9 +189,11 @@ void conv_layer(network* net, layer *l) {
                 if(relu && val<=0.f){
                     // index = co * (l->w/s) * (l->h/s) + (l->w/s)*(h/s) + w/s;
                     *out++ = 0.f; // out as stream instead of index
+                    printf("%f, ", 0.0);
                 }
                 else {
                     *out++ = val;
+                    printf("%f, ", val);
                 }
             }
         }
@@ -221,8 +226,8 @@ void max_layer(network* net, layer *l) {
     float *din =(float*)(net->data + l->din);
     float *out =(float*)(net->data + l->dout);
     for (c=0; c<l->c; c++) {
-        for (h=0; h<l->h/l->s; h++) {
-            for (w=0; w<l->w/l->s; w++) { // stride
+        for (h=0; h<l->ho; h++) {
+            for (w=0; w<l->wo; w++) { // stride
                 float val = FLT_MIN;
                 for (kh=0; kh<l->k; kh++) {
                     for (kw=0; kw<l->k; kw++) {
@@ -239,7 +244,7 @@ void max_layer(network* net, layer *l) {
 //    FILE   *f;
 //    f = fopen("pool1.npy", "rb");
 //    fseek(f, 128, SEEK_SET);
-//    long s = l->co*(l->h/l->s)*(l->w/l->s);
+//    long s = l->co*(l->ho)*(l->wo);
 //    float *dat = malloc(sizeof(float)*s);
 //    long ll = fread(dat, sizeof(float), s, f);
 //    fclose(f);
@@ -261,11 +266,11 @@ void gavg_layer(network* net, layer *l) {
         }
         *(out++) = sum/size;
     }
-    out =(float*)(net->data + l->dout);
-    for (int i = 0; i < 1000; ++i)
-    {
-        printf("%f,", out[i]);
-    }
+    // out =(float*)(net->data + l->dout);
+    // for (int i = 0; i < 1000; ++i)
+    // {
+    //     printf("%f,", out[i]);
+    // }
 }
 
 void avg_layer(network* net, layer *l) {
@@ -273,8 +278,8 @@ void avg_layer(network* net, layer *l) {
     float *din =(float*)(net->data + l->din);
     float *out =(float*)(net->data + l->dout);
     for (c=0; c<l->c; c++) {
-        for (h=0; h<l->h; h+=l->s) {
-            for (w=0; w<l->w; w+=l->s) { // stride
+        for (h=0; h<l->ho; h++) {
+            for (w=0; w<l->wo; w++) { // stride
                 float val = 0;
                 for (kh=0; kh<l->k; kh++) {
                     for (kw=0; kw<l->k; kw++) {
@@ -296,20 +301,24 @@ void network_forword(network* net){
     int i;
     net->dw = net->data;
 //    for (i=0; i<net->llen; i++) {
-    for (i=0; i<2; i++) {
+    for (i=0; i<1; i++) {
         printf("%d: ", i);
         net->layers[i].forword(net, net->layers+i);
         printf("\n");
     }
 }
 
-void build_cat(network* net, uint32_t type, uint16_t co, layer* l1, layer* l2){
+void build_cat(network* net, uint32_t type, layer* l1, layer* l2){
     layer *l = net->layers + net->llen;
+    l->c = l1->c;
     l->w = l1->w;
     l->h = l1->h;
+    l->wo = l1->wo;
+    l->ho = l1->ho;
     l->s = 1;
-    l->co = co;
+    l->co = l1->co + l2->co;
     l->type = type;
+    l->din = l1->din;
     l->dout = l1->dout;
     l->forword = cat_layer;
     strcpy(l->name, "fcat");
@@ -329,7 +338,7 @@ void build_fire(network* net, uint16_t inplanes, uint16_t squeeze_planes, uint16
     strcpy(l3->name, "f3x3");
     // concat: expand1x1 + expand3x3
     // *may use memery composition instead of concat layer: channel data + channel data
-    build_cat(net, L_CONCAT, expand1x1_planes+expand3x3_planes, l1, l3);
+    build_cat(net, L_CONCAT, l1, l3);
 }
 
 /* SqueezeNet v1.1 */
@@ -352,8 +361,8 @@ void build_SqueezeNet_v11(network* net){
     build_fire(net, 512, 64, 256, 256);
 
     // Final convolution
-    build_layer(net, L_CONV|L_RELU, net->classes, 1, 1, 0, 1);
-    build_layer(net, L_AVG, net->classes, 14, 14, 0, 1);
+    layer *l=build_layer(net, L_CONV|L_RELU, net->classes, 1, 1, 0, 1);
+    build_layer(net, L_AVG, net->classes, l->wo, l->wo, 0, 1);
 }
 
 /* SqueezeNet v1.0 */
@@ -372,13 +381,14 @@ void load_weight(network* net, const char* path){
     // printf("%d,%d\n", net->weight, weight);
 }
 
-void load_image(network* net, const char* path){
+void load_image_(network* net, const char* path){
     int w, h, c;
-    unsigned char *img = stbi_load(path, &w, &h, &c, 0);
+    uint8_t *img = stbi_load(path, &w, &h, &c, 0);
+    uint8_t *img_ = img;
     float *dat = net->data + net->weight;
-    uint8_t temp[net->input_w * net->input_h * net->input_c];
+    uint8_t temp[w*h*c];
     // resize
-    stbir_resize_uint8(img, w, h, 0, temp, net->input_w, net->input_h, 0, net->input_c);
+    stbir_resize_uint8(img, w, h, 0, temp, net->input_w, net->input_h, 0, 3);
     // stbi_write_bmp("cat_227.bmp", net->input_w, net->input_h, 3, temp); // test
     // convert to C * W * H, float
     // mean-subtracted values:
@@ -386,19 +396,55 @@ void load_image(network* net, const char* path){
     for (c=0; c<net->input_c; c++) {
         for (h=0; h<net->input_h; h++) {
             for (w=0; w<net->input_w; w++) {
-                *dat++ = (float)temp[net->input_w*3*h + w*3 + (2-c)] - BGR[c];
+                *dat++ = temp[net->input_w*3*h + w*3 + (2-c)] - BGR[c];
             }
         }
     }
-//    dat = net->data + net->weight;
-//    for (c=0; c<net->input_c; c++) {
-//        for (h=0; h<net->input_h; h++) {
-//            for (w=0; w<net->input_w; w++) {
-//                    printf("%f, ", *dat++);
-//                }
-//            }
-//    }
+    dat = net->data + net->weight;
+    for (c=0; c<net->input_c; c++) {
+        for (h=0; h<net->input_h; h++) {
+            for (w=0; w<net->input_w; w++) {
+                   printf("%f, ", *dat++);
+                }
+            }
+    }
+}
+
+void load_image(network* net, const char* path){
+    int w, h, c;
+    uint8_t *img = stbi_load(path, &w, &h, &c, 0);
+    uint8_t *img_ = img;
+    float *dat = net->data + net->weight;
+    float *tem = malloc(w*h*c*sizeof(float)), *tem_=tem;
+    for (int i = 0; i < h*w*c; ++i){
+        *tem_++ = *img_++;
+    }
     stbi_image_free(img);
+    // resize
+    float *temp = malloc(net->input_w * net->input_h * net->input_c * sizeof(float));
+    stbir_resize_float(tem, w, h, 0, temp, net->input_w, net->input_h, 0, net->input_c);
+
+    // stbi_write_bmp("cat_227.bmp", net->input_w, net->input_h, 3, temp); // test
+    // convert to C * W * H, float
+    // mean-subtracted values:
+    float BGR[] = {104.00698793, 116.66876762, 122.67891434};
+    for (c=0; c<net->input_c; c++) {
+        for (h=0; h<net->input_h; h++) {
+            for (w=0; w<net->input_w; w++) {
+                *dat++ = temp[net->input_w*3*h + w*3 + (2-c)] - BGR[c];
+            }
+        }
+    }
+    dat = net->data + net->weight;
+    for (c=0; c<net->input_c; c++) {
+        for (h=0; h<net->input_h; h++) {
+            for (w=0; w<net->input_w; w++) {
+                   printf("%f, ", *dat++);
+                }
+            }
+    }
+    // free(tem);
+    // free(temp);
 }
 
 void load_img_npy(network* net, const char* path){
@@ -437,11 +483,11 @@ int main(int argc, const char * argv[]) {
 
     load_weight(&net_v11, argv[1]);
     load_label(&net_v11, argv[2]);
-//    load_image(&net_v11, argv[3]); // raw image, [c * w * h]
-    load_img_npy(&net_v11, argv[3]);
-    build_SqueezeNet_v11(&net_v11);
-//    print_wetwork(&net_v11);
-    network_forword(&net_v11);
+    load_image(&net_v11, argv[3]); // raw image, [c * w * h]
+    // load_img_npy(&net_v11, argv[3]);
+    // build_SqueezeNet_v11(&net_v11);
+   // print_wetwork(&net_v11);
+    // network_forword(&net_v11);
     
     printf("\n");
     return 0;
